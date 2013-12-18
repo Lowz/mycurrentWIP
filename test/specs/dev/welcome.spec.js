19d287de6a7c9c1f7ba238842f6c1bbb66a8dbad
Lawrence <lawrencewheeldon@fluentmoney.co.uk>
2013-12-18 13:33:33 +0000

    all tests working, pre tidy up

--- test/specs/dev/welcome.spec.js
+++ test/specs/dev/welcome.spec.js
@@ -1,30 +1,36 @@
 /*global jasmine, describe, beforeEach, it, expect, require */
 describe('viewmodels/welcome', function () {
     "use strict";
-    requirejs.config({
-        baseUrl: '/app'
-    });
+    //requirejs.config({
+    //    baseUrl: '/app'
+    //});
 
     var Welcome = require('viewmodels/welcome');
     var app = require('durandal/app');
     var viewLocator = require('durandal/viewLocator');
     var compo;
     var flag;
+    var fuckupfind;
 
     it('should have a dom', function () {
-
+        fuckupfind = 'before the runs';
         runs(function () {
             flag = false;
+            fuckupfind = 'before app start';
             app.start().then(function () {
+                fuckupfind = 'after app start';
                 //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
                 //Look for partial views in a 'views' folder in the root.
                 viewLocator.useConvention();
 
                 //Show the app by setting the root view model for our application with a transition.
                 app.setRoot('../../../app/viewmodels/welcome', 'entrance');
+                fuckupfind = 'before app on';
                 app.on('test:compositionComplete').then(function (obj) {
+                    fuckupfind = 'after app on';
                     compo = obj;
                     if (compo !== undefined) {
+                        fuckupfind = 'inside the if';
                         flag = true;
                         app.off();
                     }
@@ -34,10 +40,10 @@ describe('viewmodels/welcome', function () {
 
         waitsFor(function () {
             return flag;
-        }, 'compo didnt come back :(');
+        }, fuckupfind);
 
         runs(function () {
-            expect(compo.view.childNodes[1].innerHTML).toBe('not correct');
+            expect(compo.view.childNodes[1].innerHTML).toBe('hello?');
         });
     });
 });
